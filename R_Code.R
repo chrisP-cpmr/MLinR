568f1c52a527c2bac862995e6d1bb27c1c523027
chrisP-cpmr <59762271+chrisP-cpmr@users.noreply.github.com>
2021-05-07 16:01:02 +0200

    Template 10-fold

--- R_Code.R
+++ R_Code.R
@@ -9,6 +9,12 @@ library(multcomp)
 library(splines)
 library(faraway)
 library(dplyr)
+library(caret)
+library(tidyverse)
+library(neuralnet)
+library(nnet)
+library(gamlss.add)
+library(ggplot2)
 
 # ---------- Read / inspect data -----------------------------------------------
 
@@ -111,8 +117,102 @@ ggplot(data = df.airbnb,
 # bedrooms
 # beds
 
+## glm Possion
 
+# simple model
+glm.accommodates.1 <- glm(accommodates ~ log_price + property_type + amenities_Gym +
+                          amenities_Breakfast + bed_type + city, 
+                        family = "poisson",
+                        data = df.airbnb)
 
+summary(glm.accommodates.1)
+summary(glm.accommodates.1)$r.squared
+
+# complex model
+# removing 
+glm.accomodates.2 <- glm(accommodates ~ property_type[17] + review_scores_rating + 
+                           bedrooms + amenities_Pets + city + (property_type + bedrooms) * 
+                           city, 
+                         family="poisson",
+                         data = df.airbnb)
+
+summary(glm.accomodates.2)
+summary(glm.accomodates.2)$r.squared
+
+# define the test train split
+set.seed(123)
+indices <- createDataPartition(df.airbnb$log_price, p=.85, list = F)
+
+# set train and test data
+df.airbnb.train <- df.airbnb %>% 
+  slice(indices)
+df.airbnb.test <- df.airbnb %>%
+  slice(-indices)
+
+# fit the model with train data
+glm.accommodates.1.train <- glm(formula = formula(glm.accommodates.1),
+                                data = df.airbnb.train)
+
+# predict with test data
+predicted.accomodates.1.test <- predict(glm.accommodates.1.train, 
+                                        newdata = df.airbnb.test)
+
+# compute R^2
+cor(predicted.accomodates.1.test, df.airbnb.test$accommodates)^2
+
+# the same for the complex model
+# fit the model with train data
+glm.accommodates.2.train <- glm(formula = formula(glm.accomodates.2),
+                                data = df.airbnb.train)
+
+# predict with test data
+predicted.accomodates.2.test <- predict(glm.accommodates.2.train, 
+                                        newdata = df.airbnb.test)
+
+# compute R^2
+cor(predicted.accomodates.2.test, df.airbnb.test$accommodates)^2
+
+# result, the complex model models the data far better
+
+# lets run several cross validation checks
+set.seed(234)
+r.squared.simple <- c()
+r.squared.complex <- c()
+
+for(i in 1:10^2){
+  ## prepare data
+  indices <- createDataPartition(df.airbnb$log_price, p=.85, list = F)
+  df.airbnb.train <- df.airbnb %>% 
+    slice(indices)
+  df.airbnb.test <- df.airbnb %>%
+    slice(-indices)
+  ##create the simple model
+  glm.accommodates.1.train <- glm(formula = formula(glm.accommodates.1),
+                                  data = df.airbnb.train)
+  # predict with test data
+  predicted.accomodates.1.test <- predict(glm.accommodates.1.train, 
+                                          newdata = df.airbnb.test)
+  # compute R^2
+  r.squared.simple[i] <- cor(predicted.accomodates.1.test, 
+                             df.airbnb.test$accommodates)^2
+  ##create complex model
+  # fit the model with train data
+  glm.accommodates.2.train <- glm(formula = formula(glm.accomodates.2),
+                                  data = df.airbnb.train)
+  # predict with test data
+  predicted.accomodates.2.test <- predict(glm.accommodates.2.train, 
+                                          newdata = df.airbnb.test)
+  # compute R^2
+  r.squared.complex <- cor(predicted.accomodates.2.test, 
+                           df.airbnb.test$accommodates)^2
+}
+
+mean(r.squared.simple)
+mean(r.squared.complex)
+
+# the mean of the more complex model is consistently better
+
+boxplot(r.squared.simple, r.squared.complex)
 
 
 # ------------ Fitting first model ---------------------------------------------
@@ -142,3 +242,75 @@ lm.airbnb.2 <- lm(log_price ~ property_type + room_type + accommodates + bathroo
 summary(lm.airbnb.2)
 
 anova(lm.airbnb.1, lm.airbnb.2)
+
+# ------------------- Creating a ANN -----------------------------
+
+boxplot(log_price ~ property_type)
+boxplot(log_price ~ city)
+ggplot(data = df.airbnb,
+       mapping = aes(y = log_price,
+                     x = review_scores_rating)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  facet_wrap(. ~ city)
+
+testIndexes.NN <- which(folds==1, arr.ind = TRUE)
+testData.NN <- df.airbnb[testIndexes.NN, ]
+trainData.NN <- df.airbnb[-testIndexes.NN, ]
+
+price.NN <- nnet(log_price ~ ., data = trainData.NN, size=10, maxit=100, range=5, decay=5e-4)
+plot(price.NN)
+price.NN
+
+## make a prediction
+pred.NN <- predict(price.NN, testData.NN, type = "raw")
+cm.NN <- table(pred=pred.NN, true=testData.NN$log_price)
+cm.NN
+
+
+## 2nd try
+max <- apply(df.airbnb, 2, max)
+min <- apply(df.airbnb, 2, min)
+airbnb_scaled <- as.data.frame(scale(df.airbnb, center = min, scale = max - min))
+airbnb_net = neuralnet(log_price ~ . , trainData.NN, hidden = 3 , linear.output = TRUE)
+plot(cereal_net)
+
+# ------------------- Creating a 10-fold cross validation template ------------------
+
+# 10-fold cross validation
+set.seed(111)
+r.squared.simple <- c()
+r.squared.complex <- c()
+# shuffle data
+df.airbnb <- df.airbnb[sample(nrow(df.airbnb)),]
+folds <- cut(seq(1,nrow(df.airbnb)), breaks = 10, labels = FALSE)
+for(i in 1:10){
+  testIndexes <- which(folds==i, arr.ind = TRUE)
+  testData <- df.airbnb[testIndexes, ]
+  trainData <- df.airbnb[-testIndexes, ]
+  ## insert your models - simple
+  # fit the model with test data
+  model.1.train <- glm(formula = formula(glm.accommodates.1),
+                       data = df.airbnb.train)
+  # predict the model
+  predicted.model.1.test <- predict(model.1.train,
+                                    newdata = df.airbnb.test)
+  # compute R^2
+  r.squared.simple[i] <- cor(predicted.model.1.test, 
+                             df.airbnb.test$accommodates)^2
+  ## insert you model - complex
+  # fit the model with test data
+  model.2.train <- glm(formula = formula(glm.accomodates.2),
+                       data = df.airbnb.train)
+  # predict the model
+  predicted.model.2.test <- predict(model.2.train,
+                                    newdata = df.airbnb.test)
+  # compute R^2
+  r.squared.complex <- cor(predicted.model.2.test, 
+                           df.airbnb.test$accommodates)^2
+}
+
+mean(r.squared.simple)
+mean(r.squared.complex)
+
+
